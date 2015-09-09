2482a4e1a7d3c2b383a6fd169ef9020e449c36bc
Ben Lenington <ben2lening@gmail.com>
2015-09-09 12:02:10 -0500

    Secure User Database (Rev 1)
    Create a test passwords file on the C drive. Structural Changes.

--- Database/src/Database.java
+++ Database/src/Database.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class Database {
 	 static String[][] passwords;
+	 
+
 	
 	
 	public static void main(String args[]){
@@ -11,6 +14,21 @@ public class Database {
 		System.out.printf("------Enter Password------\n");
 		String tmp1 = s.nextLine();
 		System.out.printf("Echo User: %s,  Pass:%s", tmp,tmp1);
+		
+		for (int i = 0; i<passwords.length; i++){
+			System.out.printf("loop");
+			
+		}
+		System.out.printf("File Test\n");
+		load();
+		
+		
+	}
+	
+	static public void load(){
+		File f = new File("C://Windows/test.txt");
+		return; 
+			
 		
 	}
 	
