9fbc8ca0302018dd87aebc237af5e26d99b4fea4
Ben Lenington <ben2lening@gmail.com>
2015-09-16 16:12:20 -0500

    Secure User Database (Rev 5)
    Change visibility of pass to private and create getter and setter
    methods in addition to revising Database.java methods.

--- Database/src/Users.java
+++ Database/src/Users.java
@@ -1,4 +1,23 @@
 
 public class Users {
+	public String name;
+	private String pass;
+	public String message;
+	
+	 void deleteUser(){
+		name = "";
+		pass = "";
+		message = "";
+	}
+
+
+public String getPass(){
+	return pass;
+}
+
+public void setPass(String p){
+	pass = p;
+	System.out.println("Setter: Pass changed");
+}
 
 }
