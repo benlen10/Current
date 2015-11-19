8509ddd705ad249b7886873aea3a8d1db64586de
Ben Lenington <ben2lening@gmail.com>
2015-11-19 10:24:07 -0600

    UniCade Cmd
    (Alpha 1)
    Initial program structure

---
+++ UniCadeCmd/UniCadeCmd/Console.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniCadeCmd
+{
+    class Console
+    {
+        private string name;
+        private string emuPath;
+        private string romPath;
+        private string prefPath;
+    }
+}
---
+++ UniCadeCmd/UniCadeCmd/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniCadeCmd
+{
+    class Game
+    {
+        private string title;
+        private string filename;
+        private int releaseDate;
+        private int launchCount;
+        private string publisher;
+        private string developer;
+        private string[] geners;
+        private string[] tags;
+        private int userScore;
+        private int CriticScore;
+        private string trivia;
+        private int players;
+        private string esrb;
+        private string esrbDescriptor;
+
+
+    }
+}
