60236faffa3d2551f403e8ce622cc459144e9d4c
Ben Lenington <ben2lening@gmail.com>
2015-11-26 13:45:54 -0600

    UniCade Cmd (Alpha 13)
    Implement constructor and additional get/set methods for the Review
    class

--- UniCadeCmd/UniCadeCmd/Review.cs
+++ UniCadeCmd/UniCadeCmd/Review.cs
@@ -14,5 +14,63 @@ namespace UniCadeCmd
         private string reviewText;
         private int reccomend;
 
+        public Review(string console, string gameTitle, int score, string reviewText, int reccomend)
+        {
+            this.console = console;
+            this.gameTitle = gameTitle;
+            this.score = score;
+            this.reviewText = reviewText;
+            this.reccomend = reccomend;
+        }
+
+        public string getConsole()
+        {
+            return console;
+        }
+
+        public string getTitle()
+        {
+            return gameTitle;
+        }
+
+        public int getScore()
+        {
+            return score;
+        }
+
+        public int getReccomend()
+        {
+            return reccomend;
+        }
+
+        public void setConsole(string s)
+        {
+            console = s;
+        }
+
+        public void setTitle(string s)
+        {
+            gameTitle = s;
+        }
+
+        public void setScore(int s)
+        {
+            score = s;
+        }
+
+        public void setReviewText(string s)
+        {
+            reviewText = s;
+        }
+
+        public void setReccomend(int s)
+        {
+            reccomend = s;
+        }
+
+
+
     }
+
+    
 }
