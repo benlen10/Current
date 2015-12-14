b3a118ec7b3e76606fe0b2ec88ed3f7cbf5ddba2
Ben Lenington <ben2lening@gmail.com>
2015-12-14 10:41:15 -0600

    UniCade Cmd (Alpha 33)
    Implement parse publisher

--- UniCadeCmd/UniCadeCmd/WebOps.cs
+++ UniCadeCmd/UniCadeCmd/WebOps.cs
@@ -11,7 +11,7 @@ namespace UniCadeCmd
     {
         public static void scrapeInfo(Game g)
         {
-            string url = ("http://www.mobygames.com/game/" + g.getConsole() +"/" + g.getTitle().Replace(" ", "-"));
+            string url = ("http://www.mobygames.com/game/" + g.getConsole() + "/" + g.getTitle().Replace(" ", "-"));
             url = url.ToLower();
             //System.Console.WriteLine(url);
             Uri uri = new Uri("http://www.mobygames.com/game/resident-evil-2");
@@ -31,7 +31,8 @@ namespace UniCadeCmd
                 indexA = 0;
             }
             string s = html.Substring(indexA, indexA + 100);
-            if (s.Contains("Everyone")){
+            if (s.Contains("Everyone"))
+            {
                 g.setEsrb("Everyone");
             }
             else if (s.Contains("Kids to Adults"))
@@ -42,16 +43,20 @@ namespace UniCadeCmd
             {
                 g.setEsrb("Everyone 10+");
             }
-            else if (s.Contains("Teen")){
+            else if (s.Contains("Teen"))
+            {
                 g.setEsrb("Teen");
             }
-            else if (s.Contains("Mature")){
+            else if (s.Contains("Mature"))
+            {
                 g.setEsrb("Mature");
             }
-            else if (s.Contains("Mature")){
+            else if (s.Contains("Mature"))
+            {
                 g.setEsrb("Mature");
             }
-            else if (s.Contains("Adults Only")){
+            else if (s.Contains("Adults Only"))
+            {
                 g.setEsrb("AO (Adults Only)");
             }
 
@@ -70,14 +75,51 @@ namespace UniCadeCmd
 
             if (tmp > 0)
             {
-                int indexB = html.IndexOf("release-info", (tmp+20));
+                int indexB = html.IndexOf("release-info", (tmp + 20));
 
-                string releaseDate = html.Substring((indexB+14), 12);
+                string releaseDate = html.Substring((indexB + 14), 12);
                 g.setReleaseDate(releaseDate);
-                    System.Console.WriteLine(g.getReleaseDate());
+                System.Console.WriteLine(g.getReleaseDate());
             }
-           
-            
+
+            //Parse User and Critic Scores
+            tmp = 0;
+            tmp = html.IndexOf("scoreHi");
+            //System.Console.WriteLine(tmp);
+
+            if (tmp > 0)
+            {
+                string criticScore = html.Substring((tmp + 9), 2);
+                g.setCriticScore(criticScore);
+                System.Console.WriteLine(g.getCriticScore());
+                int indexB = html.IndexOf("ScoreH", (tmp + 20));
+                if (indexB > 0)
+                {
+                    string userScore = html.Substring((indexB + 9), 2);   //Fix parsing userscore
+                    g.setUserScore(userScore);
+                    System.Console.WriteLine(userScore);
+
+                }
+            }
+            //Parse Publisher
+            tmp = 0;
+            tmp = html.IndexOf("/company/");
+            if (tmp > 0)
+            {
+                int tmp2 = html.IndexOf("\"<", tmp);
+                //System.Console.WriteLine(tmp);
+
+
+
+                    string publisher = html.Substring((tmp + 9), 10);
+                    g.setPublisher(publisher);
+                    System.Console.WriteLine(publisher);
+                    int indexB = html.IndexOf("ScoreH", (tmp + 20));
+
+
+
+            }
+
 
         }
     }
