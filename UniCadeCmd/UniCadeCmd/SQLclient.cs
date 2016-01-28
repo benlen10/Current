c27dc84e4ecde42abb6e418ae13361e0c592adf1
Ben Lenington <ben2lening@gmail.com>
2016-01-28 12:55:35 -0600

    UniCade Interface (Beta 81)
    Create a new SQLclient class to interact with a remote SQL database

---
+++ UniCadeCmd/UniCadeCmd/SQLclient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace UniCadeCmd
+{
+    class SQLclient
+    {
+
+        public static bool connectSQL()
+        {
+            SqlConnection myConnection = new SqlConnection("user id=root;" + "password=Star6120;server=serverurl;" + "Trusted_Connection=yes;" + "database=database; " + "connection timeout=30");
+
+            try
+            {
+                myConnection.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.ToString());
+                return false;
+            }
+
+        }
+    }
+}
