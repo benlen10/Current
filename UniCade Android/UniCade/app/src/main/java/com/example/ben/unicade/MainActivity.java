4eb46b75bc83c6e1a33b43ffdbebdadb1ec900a8
Ben Lenington <ben2lening@gmail.com>
2016-02-29 10:46:53 -0600

    UniCade Android (Alpha 11)
    Implement popup text AlertDialog

--- UniCade Android/UniCade/app/src/main/java/com/example/ben/unicade/MainActivity.java
+++ UniCade Android/UniCade/app/src/main/java/com/example/ben/unicade/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.ben.unicade;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -45,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     public static TextView t6;
     public static TextView t7;
     public static ImageView i1;
+    private String m_Text = "";
 
 
 
@@ -201,7 +207,35 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(dataAdapter);
     }
 
+    public void showInputDialog(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Title");
 
+// Set up the input
+        final EditText input = new EditText(this);
+// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        builder.setView(input);
+
+// Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
 
 
 }
+
+
+
