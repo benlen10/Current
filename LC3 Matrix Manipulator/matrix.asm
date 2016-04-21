1a9320bbd23e82a7f9d444145e94234d67b03b6a
Ben Lenington <ben2lening@gmail.com>
2016-04-21 14:41:15 -0500

    LC3 Matrix (Beta 2)
    Implement right shift functionality

--- LC3 Matrix Manipulator/matrix.asm
+++ LC3 Matrix Manipulator/matrix.asm
@@ -34,7 +34,31 @@ ADD R1, R0, #-5
 BRz TRANSPOSE
 BR DONE
 
+RSHIFT 
+LDI R1, Shift
+LDI R2, Cols
+LDI R3, Rows
+ADD R0, R1, R2 ;Check if amount to shift = width
+BRz DONE
 
+LD R6, R3, #0 ;Current Row Counter
+LD R0, Matrix  ;Initialize Current position at end of beginning of matrix
+LOOP1
+LD R2, Cols  ;Reload initial col value to R2
+STI R0, EndRow  ;Load last value of the row to EndRow
+ADD R0, R0, -1 ;Decrement once outside of loop
+INLOOP1 STR R0, R0, #1   ;Save pos to (pos+1)
+ADD R0, R0, #-1 ;Shift Pointer Left
+Add R2, R2, #-1 ;Decrement col count
+BRzp INLOOP1     ;Continue inter loop until pos pointer hits beginning of row -1
+LDI R5, EndValue ;Load end value to a temp register, R5
+ADD R0, R0, #1 ;Shift R0 up 1 back to begining of row
+STI R0, EndValue ;Replace the first value of the row with EndValue
+ADD R1, R1, #1   ;Decerement Shift counter
+BRp LOOP1         ;If shift counter is above zero, loop back to outer loop1
+ADD R6, R6, #-1  ;Decrement row counter
+BRp LOOP1  ;If row counter is above zero, loop back to outer loop1
+BR DONE 
 
 
 
@@ -43,13 +67,18 @@ BR DONE
 INVALIDOP
 ;Throw Error
 
+
+
 DONE TRAP x25  
 ;Labels
-Start  .FILL x4000
-Length .FILL x5000
-Length .FILL x5001
+Rows   .FILL x5000
+Cols   .FILL x5001
 OP     .FILL x5002
 Shift  .FILL x5003
 Matrix .FILL x5004
+Start  .FILL x4000 ;Store registers in x4000 - x4007
+Tmp1   .FILL x4010 
+EndRow . FILL x4011
+
 
 	.END
