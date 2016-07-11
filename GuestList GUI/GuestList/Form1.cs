48ff34504657409f9934f454c368cb4a243e1b02
Ben Lenington <UC214636@ten.thomsonreuters.com>
2016-07-11 08:43:10 -0500

    Guest List GUI (Alpha 1)
    Initial Program Structure

---
+++ GuestList GUI/GuestList/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuestList
+{
+     partial class Form1 : Form
+    {
+         protected static string pass;
+        public static string user;
+        private string realPass = "temp";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PassPrompt passWindow = new PassPrompt();
+            passWindow.ShowDialog();
+            
+
+        }
+
+        static public void setPass(string p)
+        {
+            pass = p;
+        }
+    }
+}
