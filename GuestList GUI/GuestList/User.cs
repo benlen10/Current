542dc4e9b9a33bd8f180d51462d9f609e32af39a
Ben Lenington <UC214636@ten.thomsonreuters.com>
2016-07-20 09:46:11 -0500

    GuestList GUI (Alpha 7)
    Implement the Guest class to store specific properties

---
+++ GuestList GUI/GuestList/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestList
+{
+    class User
+    {
+        private string firstName;
+        private string lastName;
+        private string birthday;
+        private bool blacklist;
+        private int priority;
+        private int status;
+
+
+        User(string firstName, string lastName, string birthday, int priority)
+        {
+            status = 0;
+            blacklist = false;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthday = birthday;
+            this.priority = priority;
+        }
+    }
+}
