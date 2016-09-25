0e55f9d925f7ab94803e67c09dd81c18ec25dae8
Ben Lenington <ben2lening@gmail.com>
2016-09-24 19:03:10 -0500

    UniCade Android (Beta 93)
    - Resolve crash when opening WebSettings window

--- UniCade Android/UniCade/app/src/main/java/com/example/ben/unicade/WebSettings.java
+++ UniCade Android/UniCade/app/src/main/java/com/example/ben/unicade/WebSettings.java
@@ -26,7 +26,7 @@ public class WebSettings extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        setContentView(R.layout.web_settings);
         loadView();
     }
 
