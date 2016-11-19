fca839ffbf674cf6b40694d2d3eb03ac5aeaf573
Ben Lenington <ben2lening@gmail.com>
2016-11-19 11:47:40 -0600

    UniCade Interface (Beta 151)
    - Remove uncessary methods from SettingsWindow class

--- Unicade/UniCade/Properties/AssemblyInfo.cs
+++ Unicade/UniCade/Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfApplication1")]
+[assembly: AssemblyTitle("UniCade Interface")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfApplication1")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Lenington Design")]
+[assembly: AssemblyProduct("UniCade Interface")]
+[assembly: AssemblyCopyright("Lenington Design ©  2016")]
+[assembly: AssemblyTrademark("UniCade")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
