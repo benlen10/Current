c72034cb85a8ddf502a0d4904fd7f8b0cde39897
Ben Lenington <ben2lening@gmail.com>
2016-11-22 10:29:30 -0600

    Revert "UniCade Interface (Beta 151)"
    This reverts commit fca839ffbf674cf6b40694d2d3eb03ac5aeaf573.

--- Unicade/UniCade/Properties/AssemblyInfo.cs
+++ Unicade/UniCade/Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UniCade Interface")]
+[assembly: AssemblyTitle("WpfApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Lenington Design")]
-[assembly: AssemblyProduct("UniCade Interface")]
-[assembly: AssemblyCopyright("Lenington Design ©  2016")]
-[assembly: AssemblyTrademark("UniCade")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WpfApplication1")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
