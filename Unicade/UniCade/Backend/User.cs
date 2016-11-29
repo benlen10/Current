1fa958c13a3492f9f5d1315da829a6c4bf88891c
Ben Lenington <ben2lening@gmail.com>
2016-11-29 09:02:56 -0600

    UniCade Interface (Beta 161)
    Restructure user class, add regions and refactor code

--- Unicade/UniCade/Backend/User.cs
+++ Unicade/UniCade/Backend/User.cs
@@ -1,115 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections;
+﻿using System.Collections.Generic;
 
 namespace UniCade
 {
     public class User
     {
-        private string userName;
-        private string pass;
-        public int loginCount;
-        public int totalLaunchCount;
-        private string userInfo;
-        private string allowedEsrb;
-        private string email;
-        private string profPic;
-        public ArrayList favorites;
+        #region Constructors
 
-
-        //Methods
         public User(string userName, string pass, int loginCount, string email, int totalLaunchCount, string userInfo, string allowedEsrb, string profPic)
         {
-            this.userName = userName;
-            this.pass = pass;
-            this.loginCount = loginCount;
-            this.totalLaunchCount = totalLaunchCount;
-            this.userInfo = userInfo;
-            this.allowedEsrb = allowedEsrb;
-            this.email = email;
-            this.profPic = profPic;
-            favorites = new ArrayList();
+            Username = userName;
+            Pass = pass;
+            LoginCount = loginCount;
+            TotalLaunchCount = totalLaunchCount;
+            UserInfo = userInfo;
+            AllowedEsrb = allowedEsrb;
+            Email = email;
+            ProfilePic = profPic;
+            Favorites = new List<Game>();
         }
 
-        public User()
-        {
-            this.userName = "New User";
-        }
+        #endregion
 
-        public string getUsername()
-        {
-            return userName;
-        }
+        #region Properties
 
-        public string getPass()
-        {
-            return pass;
-        }
+        public string Username { get; set; }
+        private string Pass { get; set; }
+        public int LoginCount { get; set; }
+        public int TotalLaunchCount { get; set; }
+        private string UserInfo { get; set; }
+        private string AllowedEsrb { get; set; }
+        private string Email { get; set; }
+        private string ProfilePic { get; set; }
+        public List<Game> Favorites { get; set; }
 
-        public string getEmail()
-        {
-            return email;
-        }
-
-        public string getProfPic()
-        {
-            return profPic;
-        }
-
-        public int getLoginCount()
-        {
-            return loginCount;
-        }
-
-        public int getLaunchCount()
-        {
-            return totalLaunchCount;
-        }
-
-        public string getUserInfo()
-        {
-            return userInfo;
-        }
-
-        public string getAllowedEsrb()
-        {
-            return allowedEsrb;
-        }
-
-        public void setName(string s)
-        {
-            userName = s;
-        }
-
-        public void setpass(string s)
-        {
-            pass = s;
-        }
-
-        public void setEmail(string s)
-        {
-            email = s;
-        }
-
-        public void setProfPic(string s)
-        {
-            profPic = s;
-        }
-
-        public void setUserInfo(string s)
-        {
-            userInfo = s;
-        }
-
-        public void setAllowedEsrb(string s)
-        {
-            allowedEsrb = s;
-        }
-
+        #endregion
     }
-
-    }
+}
 
