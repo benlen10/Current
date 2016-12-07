2d63af72e6a5f2fdfb694d1e8a8742a3a625dec5
Ben Lenington <ben2lening@gmail.com>
2016-12-07 14:08:14 -0600

    UniCade Interface (Beta 169)
    Restructure WebObs class and add documentation for all methods

--- Unicade/UniCade/Network/WebOps.cs
+++ Unicade/UniCade/Network/WebOps.cs
@@ -7,7 +7,8 @@ namespace UniCade
 {
     class WebOps
     {
-        //Scraper Settings
+        #region Properties
+
         public static int metac = 1;
         public static int mobyg = 1;
         public static int year = 1;
@@ -23,43 +24,57 @@ namespace UniCade
         public static int boxBack = 1;
         public static int screenshot = 1;
         public static string gameName;
-
         public static int maxDescriptionLength = 5000;
 
+        #endregion
 
+        #region Public Methods
+
+        /// <summary>
+        /// Scrape game info for the specified game from online databases
+        /// </summary>
         public static bool scrapeInfo(Game g)
         {
+            //Replace invalid chars within game title
             gameName = g.Title.Replace(" - ", " ");
             gameName = gameName.Replace(" ", "-");
             gameName = gameName.Replace("'", "");
-            if (mobyg > 0)
-            {
-                if (!scrapeMobyGames(g))
-                {
-                    return false;
-                }
-            }
-            if (metac > 0)
-            {
-                if (!scrapeMetacritic(g))
-                {
-                    return false;
-                }
-            }
-            return true;
 
+            //If neither site is scraped, return false
+            if (mobyg == 0 && metac == 0)
+                return false;
+
+            //Attempt to scrape mobygames if the site setting is enabled
+            if (mobyg > 0)
+                if (!scrapeMobyGames(g))
+                    return false;
+
+            //Attempt to scrape metacritic if the site setting is enabled
+            if (metac > 0)
+                if (!scrapeMetacritic(g))
+                    return false;
+
+            //If neither site returns any errors, return true
+            return true;
         }
 
+        /// <summary>
+        /// Scrape info for the specified game from Mobygames.com
+        /// </summary>
         public static bool scrapeMobyGames(Game g)
         {
+            //Check for bad input
             if (g == null)
             {
                 MessageBox.Show("Invalid game");
                 return false;
             }
+
+            //Generate the target url and convert the game title to lower case
             string url = ("http://www.mobygames.com/game/" + g.Console + "/" + gameName);
             url = url.ToLower();
 
+            //Create a new WebClient and attempt a connection
             WebClient site = new WebClient();
             string html = "";
             try
@@ -72,7 +87,7 @@ namespace UniCade
                 return false;
             }
 
-            //Parse ESRB
+            //Parse ESRB rating from Mobygames
             if (esrb > 0)
             {
                 int indexA = html.IndexOf("ESRB");
@@ -80,70 +95,47 @@ namespace UniCade
                 {
                     indexA = 0;
                 }
-                string s = html; //html.Substring(indexA, indexA + 50);
+                string s = html; 
+                
+                //Convert the parsed text to a valid ESRB rating
                 if (s.Contains("Everyone"))
-                {
                     g.Esrb = "Everyone";
-                }
                 else if (s.Contains("Kids to Adults"))
-                {
                     g.Esrb = "Everyone (KA)";
-                }
                 else if (s.Contains("Everyone 10+"))
-                {
                     g.Esrb = "Everyone 10+";
-                }
                 else if (s.Contains("Teen"))
-                {
                     g.Esrb = "Teen";
-                }
                 else if (s.Contains("Mature"))
-                {
                     g.Esrb = "Mature";
-                }
                 else if (s.Contains("Mature"))
-                {
                     g.Esrb = "Mature";
-                }
                 else if (s.Contains("Adults Only"))
-                {
                     g.Esrb = "AO (Adults Only)";
-                }
-
-                if (g.Esrb.Length > 2)
-                {
-                    System.Console.WriteLine(g.Esrb);
-                }
-                else
-                {
-                    g.Esrb = "Unrated";
-                }
             }
 
             //Parse Release Date
             if (releaseDate > 0)
             {
+                //Locate the "release-info" tag within the HTML text
                 int tmp = html.IndexOf("release-info");
 
+                //If the parsed index is valid, set the game release date to the value of the parsed text
                 if (tmp > 0)
                 {
                     int indexB = html.IndexOf("release-info", (tmp + 20));
-
-                    string releaseDate = html.Substring((indexB + 14), 4);
-                    g.ReleaseDate = releaseDate;
-                    System.Console.WriteLine(g.ReleaseDate);
+                    g.ReleaseDate = html.Substring((indexB + 14), 4);
                 }
 
-                //Parse Critic Scores
+                //Parse Critic Score
                 tmp = 0;
                 tmp = html.IndexOf("scoreHi");
 
+                //If the parsed index is valid, set the critic score to the value of the parsed text
                 if (tmp > 0)
                 {
-                    string criticScore = html.Substring((tmp + 9), 2);
-                    string s2 = html.Substring((tmp + 9));
-                    g.CriticScore = criticScore;
-
+                    string criticScore = html.Substring((tmp + 9), 2); 
+                    g.CriticScore = html.Substring((tmp + 9));
                 }
             }
 
@@ -152,34 +144,41 @@ namespace UniCade
             {
                 int tmp = 0;
                 tmp = html.IndexOf("/company/");
+
+                //If the parsed index is valid, set the game company to the value of the parsed text
                 if (tmp > 0)
                 {
                     int tmp2 = html.IndexOf("-", tmp + 10);
-                    string publisher = html.Substring((tmp + 9), tmp2 - (tmp + 9));
-                    g.Publisher = publisher;
+                    g.Publisher = html.Substring((tmp + 9), tmp2 - (tmp + 9));
                 }
             }
 
+            //Parse description
             if (description > 0)
             {
-                //Parse Description
                 int tmp = 0;
                 tmp = html.IndexOf("Description<");
+
+                //Locate the beginning of the game description
                 if (tmp > 0)
                 {
+                    //Locate the end of the game description text
                     int tmp2 = html.IndexOf("<div class", tmp + 15);
-                    System.Console.WriteLine(tmp);
 
-                    System.Console.WriteLine(tmp2);
+                    //If the parsed index is valid, set the game description to the value of the parsed text
                     if (tmp2 > 0)
                     {
+                        //Remove invalid characters from the description
                         string description = html.Substring((tmp + 16), tmp2 - (tmp + 16));
-                        description = Regex.Replace(description, @"\t|\n|\r", " ");
-                        description = description.Replace("\"", "");
+                        description = RemoveInvalidChars(description);
+
+                        //Trim the description if it exceeds the max length
                         if (description.Length > maxDescriptionLength)
                         {
                             description = description.Substring(0, maxDescriptionLength);
                         }
+
+                        //Set the game description to the formatted string
                         g.Description = description;
                     }
                 }
@@ -187,58 +186,45 @@ namespace UniCade
             return true;
         }
 
-
+        /// <summary>
+        /// Scrape Metacritic for info related to the specific game
+        /// </summary>
         public static bool scrapeMetacritic(Game g)
         {
-            //Metacritic Scraper
-
             string metaCon = "";
+
+            //Convert the console to the string used by metacritic
             if (g.Console.Equals("PS1"))
-            {
                 metaCon = "playstation";
-            }
             else if (g.Console.Equals("N64"))
-            {
                 metaCon = "nintendo-64";
-            }
             else if (g.Console.Equals("GBA"))
-            {
                 metaCon = "game-boy-advance";
-            }
             else if (g.Console.Equals("PSP"))
-            {
                 metaCon = "psp";
-            }
             else if (g.Console.Equals("Gamecube"))
-            {
                 metaCon = "gamecube";
-            }
             else if (g.Console.Equals("Wii"))
-            {
                 metaCon = "wii";
-            }
             else if (g.Console.Equals("NDS"))
-            {
                 metaCon = "ds";
-            }
             else if (g.Console.Equals("Dreamcast"))
-            {
                 metaCon = "dreamcast";
-            }
 
-
-            string html;
-
+            //Return false if the console is not supported
             if (metaCon.Length < 1)
-            {
                 return false;
-            }
-
+            
+            //Generate the target metacritic url
             string url = ("http://www.metacritic.com/game/" + metaCon + "/" + gameName + "/details");
             url = url.ToLower();
 
+            //Generate the WebRequest from the url and set the user agent to a supported browser
             var http = (HttpWebRequest)WebRequest.Create(url);
             http.UserAgent = "Chrome";
+
+            //Attempt to connect to metacritic.com
+            string html;
             try
             {
                 var response = http.GetResponse();
@@ -249,69 +235,61 @@ namespace UniCade
             }
             catch
             {
-
                 MessageBox.Show("Connection Error");
                 return false;
             }
 
 
-            //Parse Esrb descriptors
+            //Parse ESRB descriptors
             if (esrbDescriptor > 0)
             {
                 int tmp = 0;
                 tmp = html.IndexOf("ESRB Descriptors:");
+
+                //If the parsed index is valid, set the ESRB rating to the value of the parsed text
                 if (tmp > 0)
                 {
+                    //Locate the end of the Rating tag
                     int tmp2 = html.IndexOf("</td>", tmp + 26);
-
                     if (tmp2 > 0)
                     {
-                        string descriptors = html.Substring((tmp + 26), tmp2 - (tmp + 26));
-                        g.Description = descriptors;
+                        g.EsrbDescriptor = html.Substring((tmp + 26), tmp2 - (tmp + 26));
                     }
                 }
             }
 
+            //Parse player count (Metacritic)
             if (players > 0)
-            {
-                //Parse Players (Metacritic)
+            {       
                 int tmp = 0;
                 tmp = html.IndexOf("Players");
+
+                //If the parsed index is valid, set the player count to the value of the parsed text
                 if (tmp > 0)
                 {
                     int tmp2 = html.IndexOf("<", tmp + 17);
-
                     if (tmp2 > 0)
                     {
-                        string players = html.Substring((tmp + 17), tmp2 - (tmp + 17));
-                        g.Players = players;
+                        g.Players = html.Substring((tmp + 17), tmp2 - (tmp + 17));
                     }
                 }
             }
-
-
-
-            //Parse Developer (Metacritic)
-            /*if (developer > 0)
-            { 
-                int tmp = 0;
-                tmp = html.IndexOf("/company/",6850 );
-                int tmp2 = html.IndexOf("/company/", (tmp+30));
-                System.Console.WriteLine("Length" + tmp2);
-                if (tmp > 0)
-                {
-                    //System.Console.WriteLine("YEs1");
-                    int tmp3 = html.Substring(tmp).IndexOf(">");
-                    if (true) //tmp2 > 0)
-                    {
-                        string dev = html.Substring((tmp2+9), 5);
-                        System.Console.WriteLine(dev);
-                        g.setDeveloper(dev);
-                    }
-                }
-            }
-        }*/
             return true;
             }
+
+        #endregion
+
+        #region Helper Methods
+
+        /// <summary>
+        /// Remove and replace all invalid chars from the input string
+        /// </summary>
+        private static string RemoveInvalidChars(string str)
+        {
+            str = Regex.Replace(str, @"\t|\n|\r", " ");
+            return str.Replace("\"", "");
+        }
+
+        #endregion
     }
 }
