73263245d8bc46b42c819a032c16a308f421fc56
Ben Lenington <ben2lening@gmail.com>
2017-02-17 08:06:19 -0600

    Unicade Interface (Beta 2016)
    Implement new Login Window functions

--- Unicade/Windows/LoginWindow.xaml.cs
+++ Unicade/Windows/LoginWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace UniCade.Windows
 {
@@ -19,9 +7,79 @@ namespace UniCade.Windows
     /// </summary>
     public partial class LoginWindow : Window
     {
-        public LoginWindow()
+        #region Properties
+
+        int _userType;
+
+        #endregion
+
+        /// <summary>
+        /// Public constructor for the LoginWindow instance
+        /// </summary>
+        public LoginWindow(int userType)
         {
+            _userType = userType;
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Close the current window instance
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LoginWindow_CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Attempt to authenticate user against the database credentials
+        /// </summary>
+        private void LoginWindow_LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Bad input checks
+            if ((Textbox_Username.Text == null) || (Textbox_Password.Text == null))
+            {
+                MessageBox.Show("Fields cannot be blank");
+                return;
+            }
+
+            //If the user is a SQL client, preform SQL user authentication 
+            if (_userType == 0)
+            {
+                if (SQLclient.AuthiencateUser(Textbox_Username.Text, Textbox_Password.Text))
+                {
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show(this, "Incorrect login details");
+                }
+            }
+
+            //If the user is a local account, validate the info and close the window if sucuessful 
+            else
+            {
+                foreach (User u in Database.UserList)
+                {
+                    if (u.Username.Equals(Textbox_Username.Text))
+                    {
+                        if (u.Pass.Equals(Textbox_Password.Text))
+                        {
+                            SettingsWindow._curUser = u;
+                            Close();
+                            return;
+                        }
+                        else
+                        {
+                            MessageBox.Show(this, "Incorrect Password");
+                            return;
+                        }
+                    }
+                }
+                MessageBox.Show(this, "User does not exist");
+                return;
+            }
         }
     }
 }
