829f8f10b9d61266d3d2f7e7d68abc93b1b77209
Ben Lenington <ben2lening@gmail.com>
2017-03-02 11:28:11 -0600

    UniCade Interface (218)
    Add buttons within Emulator tab

--- Unicade/Windows/SettingsWindow.xaml.cs
+++ Unicade/Windows/SettingsWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace UniCade.Windows
         {
             InitializeComponent();
         }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
