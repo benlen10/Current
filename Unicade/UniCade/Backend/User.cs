0781f7444b1f5e75c3a8a7358e3b78f1e0ff5a54
Ben Lenington <ben2lening@gmail.com>
2017-05-20 18:42:11 -0500

    UniCade Interface (Beta 292)
    - Fully comment the User Class

--- Unicade/UniCade/Backend/User.cs
+++ Unicade/UniCade/Backend/User.cs
@@ -4,12 +4,61 @@ namespace UniCade
 {
     public class User : IUser
     {
+        #region Properties
+
+        /// <summary>
+        /// The current (unique) username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The password for the current user
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// The numer of times this user has logged in
+        /// </summary>
+        public int LoginCount { get; set; }
+
+        /// <summary>
+        /// The total number of games that this user has launched
+        /// </summary>
+        public int TotalLaunchCount { get; set; }
+
+        /// <summary>
+        /// A brief description of the user
+        /// </summary>
+        public string UserInfo { get; set; }
+
+        /// <summary>
+        /// The max allowed ESRB for the current user (Parental Controls)
+        /// </summary>
+        public string AllowedEsrb { get; set; }
+
+        /// <summary>
+        /// The user's email address
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The password for the current user
+        /// </summary>
+        public string ProfilePicture { get; set; }
+
+        /// <summary>
+        /// A list of the user's favorite games
+        /// </summary>
+        public List<IGame> Favorites { get; set; }
+
+        #endregion
+
         #region Constructors
 
-        public User(string userName, string pass, int loginCount, string email, int totalLaunchCount, string userInfo, string allowedEsrb, string profPic)
+        public User(string userName, string password, int loginCount, string email, int totalLaunchCount, string userInfo, string allowedEsrb, string profPic)
         {
             Username = userName;
-            Password = pass;
+            Password = password;
             LoginCount = loginCount;
             TotalLaunchCount = totalLaunchCount;
             UserInfo = userInfo;
@@ -18,20 +67,6 @@ namespace UniCade
             ProfilePicture = profPic;
             Favorites = new List<IGame>();
         }
-
-        #endregion
-
-        #region Properties
-
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public int LoginCount { get; set; }
-        public int TotalLaunchCount { get; set; }
-        public string UserInfo { get; set; }
-        public string AllowedEsrb { get; set; }
-        public string Email { get; set; }
-        public string ProfilePicture { get; set; }
-        public List<IGame> Favorites { get; set; }
 
         #endregion
     }
