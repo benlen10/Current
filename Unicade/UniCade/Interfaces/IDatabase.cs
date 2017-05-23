9426ddf92e24e1cc12c24f5feea37c99bf71e955
Ben Lenington <ben2lening@gmail.com>
2017-05-23 11:43:18 -0500

    UniCade Interface (Beta 294)
    - Fully comment the IDatabase interface

--- Unicade/UniCade/Interfaces/IDatabase.cs
+++ Unicade/UniCade/Interfaces/IDatabase.cs
@@ -4,9 +4,28 @@ namespace UniCade
 {
     public interface IDatabase
     {
-        List<IConsole> ConsoleList { get; set; }
-        string HashKey { get; set; }
+        #region Properties
+
+        /// <summary>
+        /// The current number of games across all game consoles
+        /// </summary>
         int TotalGameCount { get; set; }
+
+        /// <summary>
+        /// The current hash key used to generate the license key
+        /// </summary>
+        string HashKey { get; set; }
+
+        /// <summary>
+        /// The list of consoles for the current database instance
+        /// </summary>
+        List<IConsole> ConsoleList { get; set; }
+
+        /// <summary>
+        /// The list of current users
+        /// </summary>
         List<IUser> UserList { get; set; }
+
+        #endregion
     }
 }
