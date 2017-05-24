a704fbb5d58d052112ff0e753cf02588b3d8ec36
Ben Lenington <ben2lening@gmail.com>
2017-05-24 14:29:33 -0500

    UniCade Interface Beta (296)
    - Refactor and add comments/regions to the GlobalKeyboardHook class

--- Unicade/UniCade/Backend/globalKeyboardHook.cs
+++ Unicade/UniCade/Backend/globalKeyboardHook.cs
@@ -8,9 +8,50 @@ namespace UniCade
 {
     public class GlobalKeyboardHook
     {
+        #region Properties
+
+        /// <summary>
+        /// A list of the currently hooked keys
+        /// </summary>
+        public List<Keys> HookedKeys = new List<Keys>();
+
+        /// <summary>
+        /// A pointer to an integer for the current hook instance
+        /// </summary>
+        IntPtr hhook = IntPtr.Zero;
+
+        /// <summary>
+        /// The KeyHandler event when a key is pressed
+        /// </summary>
+        public event KeyEventHandler KeyDown;
+
+        /// <summary>
+        /// The KeyHandler event when a key is released
+        /// </summary>
+        public event KeyEventHandler KeyUp;
+
+        /// <summary>
+        /// Function delegate for the keyboardHookProc function
+        /// </summary>
         public delegate int keyboardHookProc(int code, int wParam, ref KeyboardHookStruct lParam);
+
+        /// <summary>
+        /// A static function delegate for the keyboardHookProc function
+        /// </summary>
         private static keyboardHookProc callbackDelegate;
 
+        /// <summary>
+        /// Keyboard code constants
+        /// </summary>
+        const int WH_KEYBOARD_LL = 13;
+        const int WM_KEYDOWN = 0x100;
+        const int WM_KEYUP = 0x101;
+        const int WM_SYSKEYDOWN = 0x104;
+        const int WM_SYSKEYUP = 0x105;
+
+        /// <summary>
+        /// An instance of the keybaord hook
+        /// </summary>
         public struct KeyboardHookStruct
         {
             public int vkCode;
@@ -20,25 +61,14 @@ namespace UniCade
             public int extraInfo;
         }
 
-        //Define keyboard codes
-        const int WH_KEYBOARD_LL = 13;
-        const int WM_KEYDOWN = 0x100;
-        const int WM_KEYUP = 0x101;
-        const int WM_SYSKEYDOWN = 0x104;
-        const int WM_SYSKEYUP = 0x105;
+        #endregion
 
-        public List<Keys> HookedKeys = new List<Keys>();
-
-        IntPtr hhook = IntPtr.Zero;
-
-        public event KeyEventHandler KeyDown;
-
-        public event KeyEventHandler KeyUp;
+        #region Constructor & Descructor
 
         /// <summary>
         /// Public constructor for the GlobalKeyboardHook class
         /// </summary>
-		public GlobalKeyboardHook()
+        public GlobalKeyboardHook()
         {
             Hook();
         }
@@ -46,10 +76,12 @@ namespace UniCade
         /// <summary>
         /// destructor for the GlobalKeyboardHook class
         /// </summary>
-		~GlobalKeyboardHook()
+        ~GlobalKeyboardHook()
         {
             Unhook();
         }
+
+        #endregion 
 
         #region Public Methods
 
@@ -84,7 +116,9 @@ namespace UniCade
             callbackDelegate = null;
         }
 
-
+        /// <summary>
+        /// Hook the specified keys
+        /// </summary>
         public int HookProc(int code, int wParam, ref KeyboardHookStruct lParam)
         {
             if (code >= 0)
