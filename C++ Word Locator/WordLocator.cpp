76a36b6d31bea365af0cd8aaede4f0ab0226c195
Ben Lenington <ben2lening@gmail.com>
2017-05-27 14:14:10 -0500

    Implement new text file parsing structure

--- C++ Word Locator/WordLocator.cpp
+++ C++ Word Locator/WordLocator.cpp
@@ -2,11 +2,12 @@
 #include <string>
 #include <stdio.h>
 #include<iostream>
+#include <sstream>
 using namespace std;
 
 //Function prototypes
 void load(string str);
-void locate(string str);
+void locate(string str, int n);
 string parse(string input);
 
 
@@ -22,25 +23,44 @@ int main()
 
 		//If input contains "locate"
 		if (input.find("load") != string::npos) {
-			string filename = parse(input);
-			printf("Found: %s", filename.c_str());
-			fgets(str, 100, stdin);
+			string filename;
+			istringstream iss(input);
+			getline(iss, filename, ' ');
+			getline(iss, filename, ' ');
+			printf("FILENAME: %s", filename.c_str());
 			load(filename);
 		}
 
 		//If input contains "locate"
-		if (input.find("locate") != string::npos) {
-			string word = parse(input);
-			locate(word);
+		else if (input.find("locate") != string::npos) {
+			istringstream iss(input);
+			string word;
+			string count;
+			getline(iss, word, ' ');
+			getline(iss, word, ' ');
+			getline(iss, count, ' ');
+			if (count.length() > 0) {
+				int n = stoi(count);
+				printf("LOCATE: %s (%d)", word.c_str(), n);
+				locate(word, n);
+			}
+			else {
+				fprintf(stderr, "ERROR: Invalid command\n");
+			}
 		}
 
 		//If input contains "new"
-		if (input.find("new") != string::npos) {
+		else if (input.find("new") != string::npos) {
 		}
 
 		//If input contains "end"
-		if (input.find("end") != string::npos) {
+		else if (input.find("end") != string::npos) {
 			return 0;
+		}
+
+		//If input is not reconigized
+		else {
+			fprintf(stderr,"ERROR: Invalid command\n");
 		}
 	}
 	return 0;
@@ -51,7 +71,7 @@ void load(string str) {
 	FILE *fp;
 }
 
-void locate(string str) {
+void locate(string str, int n) {
 
 }
 
